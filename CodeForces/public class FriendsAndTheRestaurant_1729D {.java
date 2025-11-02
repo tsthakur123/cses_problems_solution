68881ebaab497afd1fabbc0db7f8400e4a485274
tsthakur123 <singh121tarun@gmail.com>
2025-11-02 11:52:22 +0530

    new sol

--- CodeForces/public class FriendsAndTheRestaurant_1729D {.java
+++ CodeForces/public class FriendsAndTheRestaurant_1729D {.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class FriendsAndTheRestaurant_1729D {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine().trim());
